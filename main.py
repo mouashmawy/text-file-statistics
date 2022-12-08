ea021eae0b5e6454efc285311d82ca06229ea1aa
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-12-08 13:57:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,46 +2,78 @@ import string
 import matplotlib.pyplot as plt
 import numpy as np
 
-#preparing letters lists
-#aAbBcC...012...789
-allLetersList=""
-for i in range(len(string.ascii_lowercase)):
-    allLetersList+=string.ascii_lowercase[i]+string.ascii_uppercase[i]
-allLetersList+=string.digits
-
-#preparing letters lists
-letterFreqDict = {}
-for letter in allLetersList:
-    letterFreqDict[letter] = 0
 
 
-#Reading text from file
-textFileName ="text.txt"
-textInFile = ""
-try:
-    file = open(textFileName, encoding = 'utf-8')
-    textInFile = file.read().replace(" ","")
-    textFileLength = len(textInFile)
-finally:
-    file.close()
+class Stat:
+    def __init__(self, filename:str):
+        self.filename = filename
+
+        self.textFileLength=0
+        self.textInFile = ""
+        self.sortedFreqList =[]
+        # preparing letters lists
+        # aAbBcC...012...789
+        allLetersList = ""
+        for i in range(len(string.ascii_lowercase)):
+            allLetersList += string.ascii_lowercase[i] + string.ascii_uppercase[i]
+        allLetersList += string.digits
+        # preparing letters lists
+        self.letterFreqDict = {}
+        for letter in allLetersList:
+            self.letterFreqDict[letter] = 0
 
 
-for letter in textInFile:
-    letterFreqDict[letter] +=1
+    def __str__(self):
+        return f"File name: {self.filename}"
+
+    def __inputFile(self):
+        # Reading text from file
+        textFileName = self.filename
+        try:
+            file = open(textFileName, encoding='utf-8')
+            self.textInFile = file.read().replace(" ", "")
+            self.textFileLength = len(self.textInFile)
+        finally:
+            file.close()
+
+    def calc(self):
+        for letter in self.textInFile:
+            self.letterFreqDict[letter] += 1
+        self.sortedFreqList = sorted(self.letterFreqDict.items(), key=lambda kv: kv[1], reverse=1)
+
+    def getHighest(self):
+        try:
+            numOfHighestFreqInput = 5  # int(input("Number of highest frequencies letters: "))
+        except:
+            print("not a number....")
+            exit(0)
+        for i in range(numOfHighestFreqInput):
+            print(f"letter {self.sortedFreqList[i][0]}:: {self.sortedFreqList[i][1]}")
 
 
 
-sortedFreqList = sorted(letterFreqDict.items(), key=lambda kv:kv[1],reverse=1)
+    def PMF(self):
+        pass
 
-try:
-    numOfHighestFreqInput = 5#int(input("Number of highest frequencies letters: "))
-except:
-    print("not a number....")
-    exit(0)
+    def CDF(self):
+        pass
+
+    def mean(self):
+        pass
+
+    def variance(self):
+        pass
 
 
-for i in range(numOfHighestFreqInput):
-    print(f"letter {sortedFreqList[i][0]}:: {sortedFreqList[i][1]}")
+
+
+
+
+
+
+
+
+
 
 
 x_letters = np.array(list(letterFreqDict.keys()))
