228be7665a49a29d836bd1802923df202d3e16af
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-12-11 02:31:59 +0200

    changed to OOP

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ class Stat:
         self.textFileLength=0
         self.textInFile = ""
         self.sortedFreqList =[]
+
+        self.x_letters = None
+        self.y_freq = None
+        self.y_PMF = None
+        self.y_CDF = None
+
         # preparing letters lists
         # aAbBcC...012...789
         allLetersList = ""
@@ -21,6 +27,15 @@ class Stat:
         self.letterFreqDict = {}
         for letter in allLetersList:
             self.letterFreqDict[letter] = 0
+
+        # Reading text from file
+        textFileName = self.filename
+        try:
+            file = open(textFileName, encoding='utf-8')
+            self.textInFile = file.read().replace(" ", "")
+            self.textFileLength = len(self.textInFile)
+        finally:
+            file.close()
 
 
     def __str__(self):
@@ -41,6 +56,28 @@ class Stat:
             self.letterFreqDict[letter] += 1
         self.sortedFreqList = sorted(self.letterFreqDict.items(), key=lambda kv: kv[1], reverse=1)
 
+        self.x_letters = np.array(list(self.letterFreqDict.keys()))
+        self.y_freq = np.array(list(self.letterFreqDict.values()))
+
+        # Generating PMF
+        self.x_numbers = np.array(list(
+            range(10, len(self.x_letters) + 10)))  # Generating a list starting 10 for a rnage in length letter list
+        self.y_PMF = self.y_freq / self.textFileLength
+        # Generating CDF
+        self.y_CDF = self.y_PMF.copy()
+        for i in range(1, len(self.y_PMF)):
+            self.y_CDF[i] = self.y_CDF[i] + self.y_CDF[i - 1]
+
+
+
+    def freqPlot(self):
+        plt.bar(self.x_letters, self.y_freq)
+        plt.show()
+        plt.plot(self.x_letters, self.y_freq)
+        plt.show()
+        pass
+
+
     def getHighest(self):
         try:
             numOfHighestFreqInput = 5  # int(input("Number of highest frequencies letters: "))
@@ -51,14 +88,23 @@ class Stat:
             print(f"letter {self.sortedFreqList[i][0]}:: {self.sortedFreqList[i][1]}")
 
 
-
     def PMF(self):
+        plt.plot(self.x_letters, self.y_PMF)
+        plt.show()
         pass
 
     def CDF(self):
+        plt.plot(self.x_letters, self.y_CDF)
+        plt.show()
         pass
 
     def mean(self):
+        mean = np.sum(self.x_numbers * self.y_PMF)
+        print(mean)
+
+        variance = np.sum(self.x_numbers ** 2 * self.y_PMF) - mean ** 2
+        # variance2 = sum((self.x_numbers-mean)**2 * PMF)
+        print(np.sum(self.x_numbers * self.y_freq) / self.textFileLength)
         pass
 
     def variance(self):
@@ -66,43 +112,9 @@ class Stat:
 
 
 
-
-
-
-
-
-
-
-
-
-
-x_letters = np.array(list(letterFreqDict.keys()))
-y_freq = np.array(list(letterFreqDict.values()))
-print(x_letters)
-# plt.bar(x_letters, y_freq)
-# plt.show()
-# plt.plot(x_letters, y_freq)
-# plt.show()
-
-
-
-
-#Generating PMF & CDF
-x_numbers = np.array(list(range(10,len(x_letters)+10))) #Generating a list starting 10 for a rnage in length letter list
-PMF = y_freq / textFileLength
-
-
-CDF = PMF.copy()
-for i in range(1, len(PMF)):
-    CDF[i]= CDF[i] + CDF[i - 1]
-
-plt.plot(x_letters, CDF)
-# plt.show()
-
-mean = np.sum(x_numbers * PMF)
-print(mean)
-
-
-variance = np.sum(x_numbers **2 * PMF) - mean**2
-# variance2 = sum((x_numbers-mean)**2 * PMF)
-print(np.sum(x_numbers*y_freq)/textFileLength)
+App = Stat("text.txt")
+App.calc()
+# App.freqPlot()
+# App.getHighest()
+App.PMF()
+App.CDF()
